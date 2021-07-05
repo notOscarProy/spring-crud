8bd36803a419cad2b0571548dc05409d460a5fc4
Oscar Villanueva <Oscar_sgp@hotmail.com>
2021-07-05 11:51:48 -0500

    Crud Final

---
+++ src/sql/tablaMySQL.sql
@@ -0,0 +1,7 @@
+create table persona_table(
+	id_persona int primary key auto_increment,
+	nombre_persona varchar(256),
+    email_persona varchar(256),
+    genero_persona varchar(256),
+    edad_persona int
+);
